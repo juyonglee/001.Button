93876d9f518b33cd62310d3dc546c6d8fc7774eb
JuYong Lee <juyonglee0208@gmail.com>
2016-09-03 13:31:19 +0900

    Initial commit

---
+++ app/src/main/java/info/zpat/goal0208/basicbutton/MainActivity.java
@@ -0,0 +1,13 @@
+package info.zpat.goal0208.basicbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
